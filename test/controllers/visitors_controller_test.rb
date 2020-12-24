876ab0bc5baa2e2fc47d43a117ed88600a2acf87
yawar sultan <sultanyawar@gmail.com>
2020-12-24 15:48:37 +0500

    devise added

---
+++ test/controllers/visitors_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class VisitorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get visitors_index_url
+    assert_response :success
+  end
+end
