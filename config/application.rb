341acf9f2a36aad3e44f61a4f19b50e6f00f98cc
yawar sultan <sultanyawar@gmail.com>
2020-12-29 18:23:59 +0500

    static assets serving

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Cardless
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.serve_static_assets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
