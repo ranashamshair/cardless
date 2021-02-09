75881637fc24554e515c9780fe3522119a8a79f0
yawar sultan <sultanyawar@gmail.com>
2021-02-09 19:14:11 +0500

    api encoding error fixed

--- app/helpers/payment/red_sys.rb
+++ app/helpers/payment/red_sys.rb
@@ -2,17 +2,16 @@ module Payment
     class RedSys
         #4548812049400004
         def charge(card_number, exp_date, cvv, amount)
-            merchant_parameters = Base64.encode64(
-                {
-                    "DS_MERCHANT_AMOUNT" => "#{amount}",
-                    "DS_MERCHANT_CURRENCY" => "#{ENV["MERCHANTCURRENCY"]}",
-                    "DS_MERCHANT_CVV2" => "#{cvv}",
-                    "DS_MERCHANT_EXPIRYDATE" => "#{exp_date}",
-                    "DS_MERCHANT_MERCHANTCODE" => "#{ENV["MERCHANTCODE"]}",
-                    "DS_MERCHANT_PAN" => "#{card_number}",
-                    "DS_MERCHANT_TERMINAL" => "#{ENV["MERCHANTTERMINAL"]}",
-                    "DS_MERCHANT_TRANSACTIONTYPE" => "0"
-                }.to_s)
+            merchant_parameters = Digest::SHA256.base64digest({
+                "DS_MERCHANT_AMOUNT" => "#{amount}",
+                "DS_MERCHANT_CURRENCY" => "#{ENV["MERCHANTCURRENCY"]}",
+                "DS_MERCHANT_CVV2" => "#{cvv}",
+                "DS_MERCHANT_EXPIRYDATE" => "#{exp_date}",
+                "DS_MERCHANT_MERCHANTCODE" => "#{ENV["MERCHANTCODE"]}",
+                "DS_MERCHANT_PAN" => "#{card_number}",
+                "DS_MERCHANT_TERMINAL" => "#{ENV["MERCHANTTERMINAL"]}",
+                "DS_MERCHANT_TRANSACTIONTYPE" => "0"
+            }.to_s)
                 
             charge_detail = {
                 "Ds_MerchantParameters": merchant_parameters,
@@ -28,7 +27,6 @@ module Payment
             curlObj.post_body = charge_detail.to_json
             curlObj.perform()
             data = curlObj.body_str
-            binding.pry
             return JSON(data)
         end
     end
