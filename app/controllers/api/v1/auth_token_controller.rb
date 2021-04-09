ec627e79362cf5be2dbb8f522d75d2dd5474f097
sufyaniqbal1622 <bcsf16m16@gmail.com>
2021-04-09 19:10:28 +0500

    token genration by api keys

--- app/controllers/api/v1/auth_token_controller.rb
+++ app/controllers/api/v1/auth_token_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::AuthTokenController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def token
       if params[:client_secret].present? && params[:client_public].present?
-        @app.user.regenerate_token
-        @app.user.save
-        render_response({ access_token: @app.user.authentication_token })
+        user = User.where(secret_key: params[:client_secret], public_key: params[:client_public]).first
+        if user.present?
+          user.regenerate_token
+          user.save
+         return render json: ({ access_token: user.authentication_token })
+        else
+          return  render json: {message: 'Unable to retrieve auth token! please check your keys', status: 401}
+        end
       else
-        render_response('Unable to retrieve auth token! please check your keys', 401) && return
+       return  render json: {message: 'Unable to retrieve auth token! please check your keys', status: 401}
       end
   end
 end
