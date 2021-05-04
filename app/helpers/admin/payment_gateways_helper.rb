43c518be758cae41140e8ee581d813effc60c7e8
sufyaniqbal1622 <bcsf16m16@gmail.com>
2021-05-04 16:22:37 +0500

    new payment gateway fixes

--- app/helpers/admin/payment_gateways_helper.rb
+++ app/helpers/admin/payment_gateways_helper.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 module Admin::PaymentGatewaysHelper
+    def default_gateway_type(type)
+      case type
+        when 'gocardless'
+          return 0
+        when 'brain_tree_payment'
+          return 1
+        when 'checkout'
+          return 2
+        when 'dwolla'
+          return 3
+        when 'securion_pay'
+          return 4
+        when '_2_checkout'
+          return 5
+        when 'rapyd'
+          return 6
+        when 'stripe'
+          return 7
+      end
+
+    end
 end
--- app/models/payment_gateway.rb
+++ app/models/payment_gateway.rb
@@ -2,6 +2,6 @@
 
 class PaymentGateway < ApplicationRecord
   has_many :merchants
-  enum type: { gocardless: 0, brain_tree_payments: 1, checkout: 2, dwolla: 3,  securion_pay: 4,
+  enum gateway_type: { gocardless: 0, brain_tree_payments: 1, checkout: 2, dwolla: 3,  securion_pay: 4,
               _2_checkout: 5,  rapyd: 6,  stripe:7 }
 end
