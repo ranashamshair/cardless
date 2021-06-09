56d6c83ccf550afbe394540ad7f7fc5eb739d2c2
ranashamshair <ranashamshair12345@gmail.com>
2021-06-09 19:52:18 +0500

    routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -58,10 +58,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'about', to: 'visitors#about'
-  get 'pricing', to: 'visitors#pricing'
-  get 'faq', to: 'visitors#faq'
-  get 'terms', to: 'visitors#terms'
-  get 'privacy', to: 'visitors#privacy'
-  get 'contact', to: 'visitors#contact'
+  # get 'about', to: 'visitors#about'
+  # get 'pricing', to: 'visitors#pricing'
+  # get 'faq', to: 'visitors#faq'
+  # get 'terms', to: 'visitors#terms'
+  # get 'privacy', to: 'visitors#privacy'
+  # get 'contact', to: 'visitors#contact'
 end
