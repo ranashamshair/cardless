2e667854a1b5eed4feb0705e49874cf8455dd45d
ranashamshair <ranashamshair12345@gmail.com>
2021-06-13 14:25:57 +0500

    inner pages design change

--- config/routes.rb
+++ config/routes.rb
@@ -58,12 +58,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # get 'about', to: 'visitors#about'
+  get 'about', to: 'visitors#about'
   # get 'pricing', to: 'visitors#pricing'
-  # get 'faq', to: 'visitors#faq'
-  # get 'terms', to: 'visitors#terms'
-  # get 'privacy', to: 'visitors#privacy'
-  # get 'contact', to: 'visitors#contact'
+  get 'faq', to: 'visitors#faq'
+  get 'terms', to: 'visitors#terms'
+  get 'privacy', to: 'visitors#privacy'
+  get 'contact', to: 'visitors#contact'
   # get 'docs', to: 'visitors#docs'
 
 end
