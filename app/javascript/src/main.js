54e1b9be3a2e19aac6cdb6797de3acdc0401e63b
ranashamshair <ranashamshair12345@gmail.com>
2021-06-20 22:16:25 +0500

    partner animation added

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -42,43 +42,6 @@ $(".fa-home").css("left", "18%");
 
   });*/
 
-  var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
-
-  function moveBackground() {
-    x += (lFollowX - x) * friction;
-    y += (lFollowY - y) * friction;
-
-    //  translate = 'translateX(' + x + 'px, ' + y + 'px)';
-    translate = 'translateX(' + x + 'px) translateY(' + y + 'px)';
-
-    $('.animate-this').css({
-      '-webit-transform': translate,
-      '-moz-transform': translate,
-      'transform': translate
-    });
-
-    window.requestAnimationFrame(moveBackground);
-  }
-
-  $(window).on('mousemove', function (e) {
-
-    var isHovered = $('.animate-this:hover').length > 0;
-
-    //if(!$(e.target).hasClass('animate-this')) {
-    if (!isHovered) {
-      var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX)),
-        lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-
-      lFollowX = (10 * lMouseX) / 100;
-      lFollowY = (30 * lMouseY) / 100;
-    }
-  });
-
-  moveBackground();
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
