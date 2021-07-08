c66dcf4ef85f8e7f3f70d225ab2cec9a2adf1afd
abdul qayyum <qayyum543@gmail.com>
2021-07-08 10:43:36 +0500

    some fixes in merchant dashbaord

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,13 +4,16 @@ class Card < ApplicationRecord
   belongs_to :user
 
   def self.neutrino_post(number)
-    url = ENV['NEUTRINO_API']
-    parameters = {
-      'user-id' => ENV['CARD_BIN_USER'],
-      'api-key' => ENV['CARD_BIN_LIST_KEY'],
-      'bin-number' => number
-    }
-    response = RestClient.post(url, parameters, { content_type: :json, accept: :json })
-    response = JSON.parse(response)
+    begin
+      url = ENV['NEUTRINO_API']
+      parameters = {
+        'user-id' => ENV['CARD_BIN_USER'],
+        'api-key' => ENV['CARD_BIN_LIST_KEY'],
+        'bin-number' => number
+      }
+      response = RestClient.post(url, parameters, { content_type: :json, accept: :json })
+      response = JSON.parse(response)
+    rescue
+    end
   end
 end
